e6224992ba4bbb9c866ec6b7471c0931538feb06
Agnes <agnes.h.rhee@gmail.com>
2026-03-24 15:54:26 -0400

    new add

---
+++ musicReporting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace musicReporting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
